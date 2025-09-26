0dc7ba782d583c70aaa1953472f7181074395c13
Joao6632 <joaovictorqueirozcosta@gmail.com>
2025-09-26 15:30:30 -0300

    começando banco de dados

---
+++ backend/barbearia/src/main/java/com/belezaraiz/barbearia/BelezaRaizApplication.java
@@ -0,0 +1,13 @@
+package com.belezaraiz.barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelezaRaizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelezaRaizApplication.class, args);
+	}
+
+}
---
+++ backend/barbearia/src/test/java/com/belezaraiz/barbearia/BelezaRaizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belezaraiz.barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelezaRaizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
